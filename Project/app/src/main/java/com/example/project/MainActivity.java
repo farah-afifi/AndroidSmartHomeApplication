dabb0f4ab000af1daf1724be41974429afc8481c
farah afifi <36455840+farah-afifi@users.noreply.github.com>
2020-11-24 11:39:38 +0000

    setting up firebase project

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.analytics.connector.AnalyticsConnector;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics FBAnalytics = FirebaseAnalytics.getInstance(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
