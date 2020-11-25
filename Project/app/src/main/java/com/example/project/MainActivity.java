39d26bbbeb81fd3063eb214a6478a107c62db8bb
farah afifi <36455840+farah-afifi@users.noreply.github.com>
2020-11-25 18:10:01 +0000

    signup using Firebase DONE finally

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -9,13 +9,31 @@ import android.view.View;
 import com.google.firebase.FirebaseApiNotAvailableException;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.analytics.connector.AnalyticsConnector;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null)
+        {
+            //TODO go to main page since user is already signed in
+        }
     }
 
     public void SignUp(View view){
--- Project/app/src/main/java/com/example/project/SignUp.java
+++ Project/app/src/main/java/com/example/project/SignUp.java
@@ -1,13 +1,23 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,15 +28,38 @@ public class SignUp extends AppCompatActivity {
     EditText Password1;
     EditText Password2;
     Button SignUp;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.addAuthStateListener(authStateListener);
+
         Email = (EditText) findViewById(R.id.EmailSignUp);
         Password1 = (EditText) findViewById(R.id.Password1);
         Password2 = (EditText) findViewById(R.id.Password2);
         SignUp = (Button) findViewById(R.id.SignUpBTN);
+    }
+    FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        }
+    };
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null)
+        {
+            //TODO go to main page since user is already signed in
+        }
     }
 
     public void SignUpCLK(View view) {
@@ -38,18 +71,47 @@ public class SignUp extends AppCompatActivity {
         Pattern pattern = Pattern.compile(regex);
 
         Matcher matcher = pattern.matcher(email);
-        if( !matcher.matches()){
-            Toast.makeText(this,"Please enter a correct email" ,Toast.LENGTH_LONG ).show();
+        if (!matcher.matches()) {
+            Toast.makeText(this, "Please enter a correct email", Toast.LENGTH_LONG).show();
+        } else if (!pass.equals(pass2)) {
+            Toast.makeText(this, "Passwords don't match! Please re-enter the password", Toast.LENGTH_LONG).show();
+        } else if(TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(),"Please enter your E-mail address",Toast.LENGTH_LONG).show();
         }
-        else if(!pass.equals(pass2)){
-            Toast.makeText(this,"Passwords don't match! Please re-enter the password" ,Toast.LENGTH_LONG ).show();
+        if(TextUtils.isEmpty(pass)){
+            Toast.makeText(getApplicationContext(),"Please enter your Password",Toast.LENGTH_LONG).show();
         }
-        else{
-            Toast.makeText(this,"An email has been sent to this email, Please confirm it" ,Toast.LENGTH_LONG ).show();
-            finish();
+        if (pass.length() == 0){
+            Toast.makeText(getApplicationContext(),"Please enter your Password",Toast.LENGTH_LONG).show();
         }
-
-
+        if (pass.length()<8){
+            Toast.makeText(getApplicationContext(),"Password must be more than 8 digit",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "An email has been sent to this email, Please confirm it", Toast.LENGTH_LONG).show();
+            createAccount(email,pass);
+        }
 
     }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //TODO go to mainpage
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
